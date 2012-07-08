2d6e83054beaf65178aeda3d2da422c862aaa120
Mason Stewart <masondesu@gmail.com>
2012-07-08 18:20:43 -0400

    Cleaning up stuff that I don't think was supposed to be in here.

--- .clojurescript-output/main.js
+++
@@ -1,8 +0,0 @@
-goog.provide('error_states_of_america');
-goog.require('cljs.core');
-goog.require('jayq.core');
-goog.require('clojure.browser.repl');
-clojure.browser.repl.connect.call(null,"http://localhost:9000/repl");
-error_states_of_america.$interface = jayq.core.$.call(null,"﷐'#interface");
-jayq.core.inner.call(null,jayq.core.css.call(null,error_states_of_america.$interface,cljs.core.ObjMap.fromObject(["﷐'background"],{"﷐'background":"blue"})),"Loading!");
-alert.call(null,"ok!");
